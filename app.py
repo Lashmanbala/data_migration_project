2a62799f478b160962ed9c992c2519d35ad0aea2
lashmanbala <lashmanbala@gmail.com>
2024-12-21 10:38:17 +0530

    Writing data into target db

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import dotenv
 import os
 from read import read_table
+from write import build_insert_query, load_table
+import psycopg2
 
 dotenv.load_dotenv()
 
@@ -8,10 +10,21 @@ endpoint = os.environ.get('SOURCE_DB_HOST')
 db_name = os.environ.get('SOURCE_DB_NAME')
 user_name = os.environ.get('SOURCE_DB_USER')
 password = os.environ.get('SOURCE_DB_PASS')
-table_name = 'orders'
-limit = 10
+table_name = 'categories'
+# limit = 10
 
-res = read_table(endpoint, db_name, user_name, password, table_name, limit)
+res = read_table(endpoint, db_name, user_name, password, table_name)
 
 print(res[0])
 print(res[1])
+
+data = res[0]
+columns = res[1]
+
+
+endpoint = os.environ.get('TGT_DB_HOST')
+user_name = os.environ.get('TGT_DB_USER')
+password = os.environ.get('TGT_DB_PASS')
+db_name = os.environ.get('TGT_DB_NAME')
+
+load_table(endpoint, db_name, user_name, password, table_name, columns, data)
---
+++ write.py
@@ -0,0 +1,44 @@
+import psycopg2
+import os
+from logging_config import logger
+
+def build_insert_query(tabl_name, columns):
+    try:
+        columns_str = ', '.join(columns)
+        column_value_placeholeders = tuple(map(lambda clm : clm.replace(clm, '%s'), columns))
+        column_value_placeholeders_str = ', '.join(column_value_placeholeders)
+        query = f'INSERT INTO {tabl_name} ({columns_str}) VALUES ({column_value_placeholeders_str})'
+        return query
+    except Exception as e:
+        logger.error(f'Error while building query string: {e}')
+
+        
+def load_table(endpoint, db_name, user_name, password, table_name, columns, data, batch_size=100):
+    try:
+        conn = psycopg2.connect( host = endpoint,
+                                 port = 5432,
+                                 user = user_name,
+                                 password = password,
+                                 database = db_name )
+        cursor = conn.cursor()
+
+        query = build_insert_query(table_name, columns)
+
+        records = []
+        counter = 0
+        for rec in data:
+            records.append(rec)
+            if counter % batch_size == 0:
+                cursor.executemany(query, records)
+                conn.commit()
+                logger.info(f'Successfully loaded batch {counter} of {table_name}')
+                records = []
+            counter += 1
+        cursor.executemany(query, records)  # for the last batch of data which is lesser than batch_size
+        conn.commit()
+    
+    except Exception as e:
+        logger.error(f'Error with exception: {e}')
+    finally:
+        cursor.close()
+        conn.close()
